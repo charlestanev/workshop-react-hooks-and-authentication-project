afa488023e9e0a9044f861907834ff576da36ec2
ELMIR TANEV <51314768+charlestanev@users.noreply.github.com>
2022-11-09 10:33:36 +0200

    asgesgse
    rgsergewsrg

---
+++ j.js
@@ -0,0 +1,3 @@
+.sass('resources/assets/sass/templates-design/lab-template-design.scss', 'css/templates-design/lab-template-design.css')
+	.sass('resources/assets/sass/templates-design/finance.scss', 'css/templates-design/finance.css')
+	.sass('resources/assets/sass/templates-design/tbl-clients.scss', 'css/templates-design/tbl-clients.css')
